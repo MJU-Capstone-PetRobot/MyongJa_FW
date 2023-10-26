096a2676d542d09c575a915a0e0726d10d437dd6
YekoNGg <myj990629@gmail.com>
2023-10-26 11:53:31 +0900

    GPS 동작 확인 완료 (실내 이슈 여부 확인 필요)

--- ESP_S/esp_s/esp_s.ino
+++ ESP_S/esp_s/esp_s.ino
@@ -18,7 +18,8 @@ void setup()
   Serial.println("*******************************************");
     
   /* GPS 통신 */
-  GPS.begin(9600, EspSoftwareSerial::SWSERIAL_8N1, 11, 12);
+  GPS.begin(9600, EspSoftwareSerial::SWSERIAL_8N1, 12, 11); // RX(12), TX(11)
+  // GPS.enableIntTx(true);
 
   /* 초음파 센서 통신 */ 
   Serial1.begin(9600, SERIAL_8N1, 41, 42); // UART1 RX(41), TX(42)
@@ -67,9 +68,8 @@ String displayBatteryDuration (float watts)
 /* GPS */
 void processGPSdata()
 {
-  while(GPS.available())
+  while(GPS.available() > 0)
   {
-    Serial.printf("GPS available \n");
     if(gps.encode(GPS.read()))
     {
       if(gps.location.isValid())
@@ -193,8 +193,8 @@ void receive_from_ultrasonic_2()
 
 void loop() 
 {
-//  receive_from_ultrasonic_1();
-//  receive_from_ultrasonic_2();
+  receive_from_ultrasonic_1();
+  receive_from_ultrasonic_2();
   processGPSdata();
 /*
   float volts, amps, watts;
